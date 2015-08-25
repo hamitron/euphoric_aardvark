ca47c76082da5473a045ccffb57e23877d826481
Hamilton Karl <hamilton@nonfatmedia.com>
2015-08-25 16:18:02 -0700

    oh hey there database design, why do you hate me?

--- app/models/activity_entry.rb
+++ app/models/activity_entry.rb
@@ -1,2 +1,3 @@
 class ActivityEntry < ActiveRecord::Base
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :activity_entries, foreign_key: :fk_activity_entry_id
+	has_many :availabilities, foreign_key: :fk_primary_user
+
+	has_secure_password
 end
